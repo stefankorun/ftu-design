7271a36e958433528091df9c42b92c3f57dd485b
korun <korun_stefan@hotmail.com>
2016-02-09 03:06:14 +0100

    views-view-fields--collaborators

---
+++ templates/views/views-view-fields--collaborators.tpl.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * @file
+ * Default simple view template to all the fields as a row.
+ *
+ * - $view: The view in use.
+ * - $fields: an array of $field objects. Each one contains:
+ *   - $field->content: The output of the field.
+ *   - $field->raw: The raw data for the field, if it exists. This is NOT output safe.
+ *   - $field->class: The safe class id to use.
+ *   - $field->handler: The Views field handler object controlling this field. Do not use
+ *     var_export to dump this object, as it can't handle the recursion.
+ *   - $field->inline: Whether or not the field should be inline.
+ *   - $field->inline_html: either div or span based on the above flag.
+ *   - $field->wrapper_prefix: A complete wrapper containing the inline_html to use.
+ *   - $field->wrapper_suffix: The closing tag for the wrapper.
+ *   - $field->separator: an optional separator that may appear before a field.
+ *   - $field->label: The wrap label text to use.
+ *   - $field->label_html: The full HTML of the label to use including
+ *     configured element type.
+ * - $row: The raw result object from the query, with all data it fetched.
+ *
+ * @ingroup views_templates
+ */
+?>
+
+<?php $image = $fields['field_referal_image']; ?>
+<?php if (!empty($image->separator)): ?>
+  <?php print $image->separator; ?>
+<?php endif; ?>
+
+<?php print $image->wrapper_prefix; ?>
+  <?php print $image->label_html; ?>
+  <a href="<?php echo $row->field_field_referal_url[0]['raw']['url'] ?>" target="_blank">
+    <?php print $image->content; ?>
+  </a>
+<?php print $image->wrapper_suffix; ?>
+
+
+<!--
+default template
+
+<?php foreach ($fields as $id => $field): ?>
+  <?php if (!empty($field->separator)): ?>
+    <?php print $field->separator; ?>
+  <?php endif; ?>
+
+  <?php print $field->wrapper_prefix; ?>
+    <?php print $field->label_html; ?>
+    <?php print $field->content; ?>
+  <?php print $field->wrapper_suffix; ?>
+<?php endforeach; ?>
+-->
+
